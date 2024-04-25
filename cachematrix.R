46e9066ad7e2ce5b34af8cde5713ceee5253fe75
vinchofti <167070657+vinchofti@users.noreply.github.com>
2024-04-25 11:10:47 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#with the first function you can create for example atze<-makeCacheMatrix()
+#and set and get a matrix as well as the inverse of the matrix with atze$set(matrix(...))
+# and atze$setinverse(solve(atze$get()))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function()
+                x
+        
+        setinverse<-function(solv) 
+                m<<-solv
+        
+        getinverse<-function()
+                m
+        
+      list(set=set,
+           get=get,
+           setinverse=setinverse,
+           getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+#this function checks if there already exists an inverse for the input function x 
+#if it does it returns that value, if not it calculates and returns it 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      v<-x$getinverse
+      if(!is.null(v())) {
+              return(v())
+      }
+      
+      x$setinverse(solve(x$get(),...))
+      x$getinverse()
 }
+
+
